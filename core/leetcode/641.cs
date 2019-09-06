2c19ca1752bf675724f962ac889b49440ad03684
Tarun Batta <tarun.batta@cascade-eng.com>
2019-09-06 16:21:03 -0700

    ahh

--- core/leetcode/641.cs
+++ core/leetcode/641.cs
@@ -50,5 +50,68 @@ using System;
 
 namespace InterviewPreperationGuide.Core.LeetCode.Solution641
 {
-    public class MyCircularDeque {​    /** Initialize your data structure here. Set the size of the deque to be k. */    public MyCircularDeque(int k) { }        /** Adds an item at the front of Deque. Return true if the operation is successful. */    public bool InsertFront(int value) { }        /** Adds an item at the rear of Deque. Return true if the operation is successful. */    public bool InsertLast(int value) { }        /** Deletes an item from the front of Deque. Return true if the operation is successful. */    public bool DeleteFront() { }        /** Deletes an item from the rear of Deque. Return true if the operation is successful. */    public bool DeleteLast() { }        /** Get the front item from the deque. */    public int GetFront() { }        /** Get the last item from the deque. */    public int GetRear() { }        /** Checks whether the circular deque is empty or not. */    public bool IsEmpty() { }        /** Checks whether the circular deque is full or not. */    public bool IsFull() { } }​/** * Your MyCircularDeque object will be instantiated and called as such
+    public class Solution
+    {
+        public void Init()
+        {
+
+        }
+    }
+
+    public class MyCircularDeque
+    {​    
+		/** Initialize your data structure here. Set the size of the deque to be k. */    
+		public MyCircularDeque(int k)
+        {
+
+        }
+
+        /** Adds an item at the front of Deque. Return true if the operation is successful. */
+        public bool InsertFront(int value)
+        {
+            return false;
+        }
+
+        /** Adds an item at the rear of Deque. Return true if the operation is successful. */
+        public bool InsertLast(int value)
+        {
+            return false;
+        }
+
+        /** Deletes an item from the front of Deque. Return true if the operation is successful. */
+        public bool DeleteFront()
+        {
+            return false;
+        }
+
+        /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+        public bool DeleteLast()
+        {
+            return false;
+        }
+
+        /** Get the front item from the deque. */
+        public int GetFront()
+        {
+            return 0;
+        }
+
+        /** Get the last item from the deque. */
+        public int GetRear()
+        {
+            return 0;
+        }
+
+        /** Checks whether the circular deque is empty or not. */
+        public bool IsEmpty()
+        {
+            return false;
+        }
+
+        /** Checks whether the circular deque is full or not. */
+        public bool IsFull()
+        {
+            return false;
+        }
+    }​
 }
--- core/leetcode/911.cs
+++ core/leetcode/911.cs
@@ -40,5 +40,25 @@ using System;
 
 namespace InterviewPreperationGuide.Core.LeetCode.Solution911
 {
-    public class TopVotedCandidate {​    public TopVotedCandidate(int[] persons, int[] times) { } public int Q(int t) { } }​/** * Your TopVotedCandidate object will be instantiated and called as such: * TopVotedCandidate obj = new TopVotedCandidate(persons, times); * int param_1 = obj.Q(t); *
+    public class Solution
+    {
+        public void Init()
+        {
+            TopVotedCandidate obj = new TopVotedCandidate(null, null);
+            int param_1 = obj.Q(1);
+        }
+    }
+
+    public class TopVotedCandidate
+    {​    
+		public TopVotedCandidate(int[] persons, int[] times)
+        {
+
+        }
+
+        public int Q(int t)
+        {
+            return 0;
+        }
+    }​
 }
