c3647633ed8fe093bf814b4b5a12f593516e5866
Tarun Batta <tarunbatta@gmail.com>
2019-10-29 22:32:02 -0700

    misc

--- core/leetcode/186.cs
+++ core/leetcode/186.cs
@@ -4,16 +4,78 @@
 186. Reverse Words in a String II
 Medium
 
-// {{ MISSING QUESTION }}
+Given an input string , reverse the string word by word. 
+
+Example:
+
+Input:  ['t','h','e',' ','s','k','y',' ','i','s',' ','b','l','u','e']
+Output: ['b','l','u','e',' ','i','s',' ','s','k','y',' ','t','h','e']
+
+Note: 
+A word is defined as a sequence of non-space characters.
+The input string does not contain leading or trailing spaces.
+The words are always separated by a single space.
+
+Follow up: Could you do it in-place without allocating extra space?
 */
 
 using System;
 
 namespace InterviewPreperationGuide.Core.LeetCode.problem186 {
-        public class Solution {
+    public class Solution {
         public void Init () {
-
+            ReverseWords (null);
+            ReverseWords (new char[] { });
+            ReverseWords (new char[] { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e' });
         }
 
+        public void ReverseWords (char[] s) {
+            if (s == null || s.Length == 0) {
+                return;
+            }
+
+            int n = s.Length;
+
+            ReverseString (s, 0, n - 1);
+            ReverseWords (s, n);
+            int end = CleanSpaces (s, n);
+        }
+
+        private void ReverseWords (char[] c, int n) {
+            int i = 0;
+            int j = 0;
+
+            while (i < n) {
+                while (i < j || i < n && c[i] == ' ') i++;
+                while (j < i || j < n && c[j] != ' ') j++;
+
+                ReverseString (c, i, j - 1);
+            }
+        }
+
+        private int CleanSpaces (char[] c, int n) {
+            int i = 0;
+            int j = 0;
+
+            while (j < n) {
+                while (j < n && c[j] == ' ') j++;
+                while (j < n && c[j] != ' ') c[i++] = c[j++];
+                while (j < n && c[j] == ' ') j++;
+
+                if (j < n) {
+                    c[i++] = ' ';
+                }
+            }
+
+            return i;
+        }
+
+        private void ReverseString (char[] c, int i, int j) {
+            while (i < j) {
+                char t = c[i];
+                c[i++] = c[j];
+                c[j--] = t;
+            }
+        }
     }
 }
